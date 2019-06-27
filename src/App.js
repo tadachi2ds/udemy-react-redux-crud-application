a29d11e0154240163b6299f79fefbf9ddd8b9e36
足立崇 <tadachi2@tadachi2-MBP.local>
2019-06-27 13:30:34 +0900

    create class component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import React, {Component} from 'react';
 // import logo from './logo.svg';
 // import './App.css';
 
-
-class App extends Component {
-  render() {
-    const dom = <h1>Hi!</h1>
-    return dom;
-  }
+const App = () => {
+  return <div>H1!</div>
 }
+
+
+
+
+// class App extends Component {
+//   render() {
+//     const dom = <h1>Hi!</h1>
+//     return dom;
+//   }
+// }
 
 /*
 function App() {
