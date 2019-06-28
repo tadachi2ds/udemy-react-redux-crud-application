cf84431e8eb1a60eacc31588ecb1585a53e847c6
足立崇 <tadachi2@tadachi2-MBP.local>
2019-06-28 14:25:06 +0900

    learn state and setState

--- src/App.js
+++ src/App.js
@@ -3,59 +3,37 @@ import PropTypes from 'prop-types'
 // import logo from './logo.svg';
 // import './App.css';
 
-const App = () => {
-  const profiles = [
-    { name: "Taro", age: 10},
-    { name: "Hahako", age: 5},
-    { name: "Saburo"}
-  ]
-  return (
-    <div>
-    {
-      profiles.map((profile, index) => {
-        return <User name={profile.name} age={profile.age} key={index} />
-      })
-    }
-    </div>
-  )
+const App = () => (<Counter></Counter>)
+
+class Counter extends Component {
+  constructor(props) {
+    super(props)
+    console.log(this.state)
+    this.state = {count:0}
+  }
+
+  handlePlusButton = () => {
+    // console.log("handlePlusButton")
+    this.setState({ count: this.state.count + 1})
+  }
+
+  handleMinusButton = () => {
+    // console.log("handlePlusButton")
+    this.setState({ count: this.state.count - 1})
+  }
+
+  render() {
+    // console.log(this.state)
+    return (
+      <React.Fragment>
+        <div>count: {this.state.count}</div>
+        <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handleMinusButton}>-1</button>
+      </React.Fragment>
+    )
+  }
 }
 
-const User = (props) => {
-  return <div>Hi , I am {props.name}, and {props.age} years old! </div>
-}
 
-User.propTypes = {
-  name: PropTypes.string,
-  age: PropTypes.number
-}
 
-// class App extends Component {
-//   render() {
-//     const dom = <h1>Hi!</h1>
-//     return dom;
-//   }
-// }
-
-/*
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload. OK!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-*/
 export default App;
