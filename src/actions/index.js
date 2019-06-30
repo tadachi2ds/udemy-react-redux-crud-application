24bd3992c1491a8014d23dbdd07aac40a94f11ec
足立崇 <tadachi2@tadachi2-MBP.local>
2019-07-01 01:06:41 +0900

    mpolemant action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+export cont INCREMENT = 'INCREMENT'
+export cont DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+  type: INCREMENT
+})
+export const decrement = () => ({
+    type: DECREMENT
+})
